d8b0665393e468b3be0fde060c15fd9736150672
rikkartx <trx943368812@gmail.com>
2021-08-17 10:55:22 +0800

    Longest Palindromic Substring ON Solution

--- 5.longest-palindromic-substring.java
+++ 5.longest-palindromic-substring.java
@@ -8,7 +8,45 @@
 class Solution {
     
     public String longestPalindrome(String s) {
-        return longestPalindromeON2(s);
+        return longestPalindromeON(s);
+    }
+
+    public String longestPalindromeON(String strings) {
+        if(strings == null || strings.length() <= 1) {
+            return strings;
+        }
+        int len = strings.length();
+        char[] s = new char[len * 2 + 1];
+        int[] p = new int[s.length];
+        for (int i = 0,j = 0; i < len;) {
+            if(j % 2 == 0) {
+                s[j++] = '#';
+            } else {
+                s[j++] = strings.charAt(i++);
+                if(i == len) {
+                    s[j] = '#';
+                }
+            }
+        }
+        int r = 0, c = 0;
+        for (int i = 1; i < s.length; i++) {
+            p[i] = r > i ? Math.min(r - i, p[c * 2 - i]) : 0;
+            while (i + p[i] + 1 < s.length && i - p[i] - 1 > -1 && s[i + p[i] + 1] == s[i - p[i] - 1]) {
+                p[i]++;
+            }
+            if(i + p[i] > r) {
+                c = i;
+                r = i + p[i];
+            }
+        }
+        int max = p[0], maxi = 0;
+        for (int i = 1; i < p.length; i++) {
+            if(p[i] > max) {
+                max = p[i];
+                maxi = i;
+            }
+        }
+        return strings.substring((maxi - max) / 2, (maxi + max) / 2);
     }
 
     public String longestPalindromeON2(String s) {
@@ -47,9 +85,3 @@ class Solution {
     }
 }
 // @lc code=end
-
-class Test {
-    public static void main(String[] args) {
-        System.out.println(new Solution().longestPalindrome("abcdefggfedcba"));
-    }
-}
