5eb227b9763d0ffafcb98777de291111b0abcb45
rikkartx <trx943368812@gmail.com>
2021-08-25 09:48:18 +0800

    Maximum Subarray

---
+++ 53.maximum-subarray.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[nums.length];
+        int max = dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
+// @lc code=end
+
