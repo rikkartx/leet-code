4dbff3d2509c1cf2f4ff9cc0dcb242c7b2d833f5
rikkartx <trx943368812@gmail.com>
2021-09-02 15:08:29 +0800

    Merge K Sorted Lists

---
+++ 23.merge-k-sorted-lists.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=23 lang=java
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) {
+            return null;
+        } else if(lists.length == 1) {
+            return lists[0];
+        }
+
+        int baseLength = 1;
+        while (baseLength < lists.length) {
+            for (int i = 0; i + baseLength < lists.length; i += baseLength * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + baseLength]);
+            }
+            baseLength <<= 1;
+        }
+        return lists[0];
+        
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) {
+            return l2;
+        } else if(l2 == null) {
+            return l1;
+        }
+
+        ListNode head = new ListNode();
+        ListNode prev = head;
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        if(l1 != null) {
+            prev.next = l1;
+        } else {
+            prev.next = l2;
+        }
+        return head.next;
+    }
+}
+// @lc code=end
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
