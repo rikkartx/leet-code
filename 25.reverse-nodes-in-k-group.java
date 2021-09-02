d6397d3ae5f19181af351a19f1c9739e26dbfdcd
rikkartx <trx943368812@gmail.com>
2021-09-02 16:45:44 +0800

    Reverse Nodes In K Group

---
+++ 25.reverse-nodes-in-k-group.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=25 lang=java
+ *
+ * [25] Reverse Nodes in k-Group
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null || k <= 1) {
+            return head;
+        }
+        ListNode dummy = new ListNode();
+        int n = 0;
+        ListNode[] queue = new ListNode[k];
+        ListNode node = head;
+        ListNode prev = null;
+        while (node != null) {
+            queue[n++] = node;
+            node = node.next;
+            if(n == k) {
+                for (int i = 0; i <= n - 1; i++) {
+                    queue[i].next = i == 0 ? node : queue[i - 1];
+                }
+                if(dummy.next == null) {
+                    dummy.next = queue[n - 1];
+                }
+                if(prev != null) {
+                    prev.next = queue[n - 1];
+                }
+                prev = queue[0];
+                n = 0;
+            }
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
